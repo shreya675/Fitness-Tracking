239136a63d119204aef4d0852d8c668b69a21e46
Shreya Jaiswal <2023eeb1245@iitrpr.ac.in>
2025-07-04 13:08:00 +0530

    Update Conf.js

--- src/conf/Conf.js
+++ src/conf/Conf.js
@@ -1,34 +1,18 @@
-const conf= {
-    appwriteUrl: import.meta.env.https://fra.cloud.appwrite.io/v1,
-    appwriteProjectId: import.meta.env.6867794d0000cc03c8d3,
-    appwriteDatabaseId: import.meta.env.6867796c0003008cd1eb,
+const conf = {
+  appwriteUrl: import.meta.env.VITE_APPWRITE_ENDPOINT,
+  appwriteProjectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  appwriteDatabaseId: import.meta.env.VITE_APPWRITE_DB_ID,
 
+  appwriteUserInfoCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+  appwriteWorkOutCollectionId: import.meta.env.VITE_APPWRITE_WORKOUT_COLLECTION_ID,
+  appwriteDailyGoalsCollectionId: import.meta.env.VITE_APPWRITE_DAILYGOAL_COLLECTION_ID,
+  appwriteWeeklyGoalsCollectionId: import.meta.env.VITE_APPWRITE_WEEKLYGOAL_COLLECTION_ID,
+  appwriteWeightProgressCollectionId: import.meta.env.VITE_APPWRITE_WEIGHT_COLLECTION_ID,
 
+  appwriteBucketId: import.meta.env.VITE_APPWRITE_BUCKET_ID,
 
-    appwriteUserInfoCollectionId: import.meta.env.6867798000031509316e,
-
-    appwriteWorkOutCollectionId: import.meta.env.68677a600010483073c2,
-
-    appwriteDailyGoalsCollectionId: import.meta.env.68677ad7002bc4721d19,
-
-    appwriteWeeklyGoalsCollectionId: import.meta.env.68677b400006ac1f5ff5,
-
-    appwriteWeightProgressCollectionId: import.meta.env.68677bbf0032989c04f4,
-
-
-    // exercise API ninja
-
-
-    exerciseApiNinja: import.meta.env.sTsNZriatDxoUdkzi16Fow==TItg3KddbLXfPlmw,
-
-
-
-
-
-
-    appwriteBucketId: import.meta.env.68677e8500044db322c3,
-
-
+  exerciseApiNinja: import.meta.env.VITE_Exercise_API,
 };
 
 export default conf;
+
